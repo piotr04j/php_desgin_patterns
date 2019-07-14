e8f0b1005d32bb2dead992bbb5253ad638931b74
Piotr <you@example.com>
2019-07-14 19:35:50 +0200

    added imports of AbstractFactory

--- index.php
+++ index.php
@@ -7,5 +7,13 @@ include('./creational/FactoryMethod/ProductTwo.php');
 include('./creational/FactoryMethod/ProductOne.php');
 include('./creational/FactoryMethod/Creator.php');
 include('./creational/FactoryMethod/index.php');
+include('./creational/AbstractFactory/Product.php');
+include('./creational/AbstractFactory/Book.php');
+include('./creational/AbstractFactory/CreatorBook.php');
+include('./creational/AbstractFactory/BookENG.php');
+include('./creational/AbstractFactory/BookFR.php');
+include('./creational/AbstractFactory/BookPL.php');
+include('./creational/AbstractFactory/index.php');
+
 
 
