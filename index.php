9bf5679611e7d6c21921164abafe1ff1d53705d2
Piotr <you@example.com>
2019-07-15 21:13:57 +0200

    refactored import

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
 
-include('./creational/Singleton/Singleton.php');
 include('./creational/Singleton/index.php');
-include('./creational/FactoryMethod/ProductSource.php');
-include('./creational/FactoryMethod/ProductTwo.php');
-include('./creational/FactoryMethod/ProductOne.php');
-include('./creational/FactoryMethod/Creator.php');
+echo '<br />';
 include('./creational/FactoryMethod/index.php');
-include('./creational/AbstractFactory/Product.php');
-include('./creational/AbstractFactory/Book.php');
-include('./creational/AbstractFactory/CreatorBook.php');
-include('./creational/AbstractFactory/BookENG.php');
-include('./creational/AbstractFactory/BookFR.php');
-include('./creational/AbstractFactory/BookPL.php');
+echo '<br />';
 include('./creational/AbstractFactory/index.php');
+echo '<br />';
 
 
 
