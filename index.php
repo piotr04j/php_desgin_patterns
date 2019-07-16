ae6dcf948bf18777c539236a582119fe50f35d8e
Piotr <you@example.com>
2019-07-16 20:37:42 +0200

    temporary removed call AbstractFactory and added Prototype

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ include('./creational/Singleton/index.php');
 echo '<br />';
 include('./creational/FactoryMethod/index.php');
 echo '<br />';
-include('./creational/AbstractFactory/index.php');
-echo '<br />';
+//include('./creational/AbstractFactory/index.php');
+//echo '<br />';
+include('./creational/Prototype/index.php');
+
 
 
 
